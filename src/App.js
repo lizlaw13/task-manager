ddb06f5e7d2f2edf53fb3e22ab5171fdaf7af92b
Elizabeth Lawrence <ca81804@delta.org>
2019-06-03 16:08:29 -0700

    Created task manager

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      current_value: "",
+      values: []
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.cancelCourse = this.cancelCourse.bind(this);
+  }
+
+  handleSubmit(event) {
+    let today = new Date();
+    let time =
+      today.getHours() + ":" + today.getMinutes() + today.getSeconds() + ":";
+    event.preventDefault();
+    let toAdd = [];
+    this.setState({
+      current_value: this.element.value
+    });
+    if (this.state.values === []) {
+      toAdd.push([this.element.value, time]);
+      this.setState({
+        values: toAdd
+      });
+    } else {
+      toAdd = this.state.values;
+      toAdd.push([this.element.value, time]);
+      this.setState({
+        values: toAdd
+      });
+    }
+  }
+
+  cancelCourse = () => {
+    this.setState({
+      current_value: ""
+    });
+  };
+
+  render() {
+    let taskList = this.state.values.map(task => {
+      return <li key={task[1]}>{task[0]}</li>;
+    });
+
+    return (
+      <div className="App">
+        <div className="task-form">
+          <h2>To Do List Manager </h2>
+          <form id="create-task-form" onSubmit={this.handleSubmit}>
+            <label>Task:</label> <br />
+            <input type="text" ref={el => (this.element = el)} />
+            <input
+              type="submit"
+              value="Submit"
+              className="submit"
+              name="cancelCourse"
+              onClick={this.cancelCourse}
+            />
+          </form>
+        </div>
+        <div className="display-tasks">
+          <section>
+            <ul>{taskList}</ul>
+          </section>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
